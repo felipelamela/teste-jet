d6f43c4b4a3195fa784e97480aa33928bebdcd24
Felipe Lamela <fe-fl@outlook.com>
2024-02-28 23:58:21 -0300

    components tbody

--- app/page.tsx
+++ app/page.tsx
@@ -17,17 +17,8 @@ export default function Home() {
       </thead>
       <tbody className="bg-white divide-y divide-gray-200">
         <tr>
-        <td className="w-1/5 px-6 py-4 text-sm flex items-center">
-          <div className="w-1/2 mr-2" style={{ width: '80px' }}>
-            <Image src="/mini.png" width={80} height={60} alt="Picture of the author" />
-          </div>
-          <div className="w-1/2">
-            <p>Mini carro</p>
-          </div>
-        </td>
-          <td className="w-1/5 px-6 text-sm py-4">Reservation Data</td>
+  
           <td className="w-1/5 px-6 text-sm py-4"><span className="bg-green-500 text-white px-2 py-1 rounded-full">disponivel</span></td>
-          <td className="w-1/5 px-6 text-sm py-4">Action Data</td>
         </tr>
         {/* Outras linhas da tabela */}
       </tbody>
